8831f055fcc1c432f1c14714ddd2047eaeceef3a
Feer <feerliu@foxmail.com>
2022-05-30 22:43:22 +0200

    22

--- boc-test/controllers/controllers.py
+++ boc-test/controllers/controllers.py
@@ -5,17 +5,17 @@
  class Boc-test(http.Controller):
      @http.route('/boc-test/boc-test/', auth='public')
      def index(self, **kw):
-         return "Hello, world"
-
-     @http.route('/boc-test/boc-test/objects/', auth='public')
-     def list(self, **kw):
-         return http.request.render('boc-test.listing', {
-             'root': '/boc-test/boc-test',
-             'objects': http.request.env['boc-test.boc-test'].search([]),
-         })
-
-     @http.route('/boc-test/boc-test/objects/<model("boc-test.boc-test"):obj>/', auth='public')
-     def object(self, obj, **kw):
-         return http.request.render('boc-test.object', {
-             'object': obj
-         })
+         return "Hello, This is Boc-test"
+ #
+ #    @http.route('/boc-test/boc-test/objects/', auth='public')
+ #    def list(self, **kw):
+ #        return http.request.render('boc-test.listing', {
+ #            'root': '/boc-test/boc-test',
+ #            'objects': http.request.env['boc-test.boc-test'].search([]),
+ #        })
+ #
+ #    @http.route('/boc-test/boc-test/objects/<model("boc-test.boc-test"):obj>/', auth='public')
+ #    def object(self, obj, **kw):
+ #        return http.request.render('boc-test.object', {
+ #            'object': obj
+ #        })
