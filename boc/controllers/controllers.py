ece35bd7f5e8fdad895bdf5a6d47a3dfd8efe76e
Feer <feerliu@foxmail.com>
2022-05-30 10:58:14 +0200

    20220530-03

--- boc/controllers/controllers.py
+++ boc/controllers/controllers.py
@@ -6,15 +6,14 @@ class Boc(http.Controller):
      def index(self, **kw):
          #return "Hello, This is a test from BOC"
          return self.pool.get('warning').info(cr, uid, title='Export imformation', message="%s products Created, %s products Updated "%(str(prod_new),str(prod_update)))
-#     @http.route('/boc/boc/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('boc.listing', {
-#             'root': '/boc/boc',
-#             'objects': http.request.env['boc.boc'].search([]),
-#         })
-
-#     @http.route('/boc/boc/objects/<model("boc.boc"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('boc.object', {
-#             'object': obj
-#         })
+     @http.route('/boc-test/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('boc.listing', {
+             'root': '/boc/boc',
+             'objects': http.request.env['boc.boc'].search([]),
+         })
+     @http.route('/boc-test/objects/<model("boc.boc"):obj>/', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('boc.object', {
+             'object': obj
+         })
--- boc/controllers/show_message_box_odoo.py
+++
@@ -1,9 +0,0 @@
-from openerp.osv import fields,osv
-from openerp.tools.translate import _
-
-class display_dialog_box(osv.osv_memory):
-    _name = "display.dialog.box"
-    _columns={
-        'text': fields.text(),
-    }
-display_dialog_box()
