8642c64f1e844a4fa9e1dd4a1d589871c5d83adb
Feer <feerliu@foxmail.com>
2022-05-31 10:19:28 +0200

    20220531

--- boc/controllers/controllers.py
+++ boc/controllers/controllers.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+import sys
+sys.path.append('C:\Program Files\Odoo 14.0.20220530\server')
 
+from odoo import http
 
 class Boc(http.Controller):
     @http.route('/boc/boc/', auth='public')
     def index(self, **kw):
         return "Hello, This a test from BOC"
 
-#     @http.route('/boc/boc/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('boc.listing', {
-#             'root': '/boc/boc',
-#             'objects': http.request.env['boc.boc'].search([]),
-#         })
+    @http.route('/boc/boc/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('boc.listing', {
+            'root': '/boc/boc',
+            'objects': http.request.env['boc.boc'].search([]),
+        })
 
-#     @http.route('/boc/boc/objects/<model("boc.boc"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('boc.object', {
-#             'object': obj
-#         })
+    @http.route('/boc/boc/objects/<model("boc.boc"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('boc.object', {
+            'object': obj
+        })
--- boc/models/models.py
+++ boc/models/models.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
 
+class boc(models.Model):
+    _name = 'boc.boc'
+    _description = 'boc.boc'
 
-# class boc(models.Model):
-#     _name = 'boc.boc'
-#     _description = 'boc.boc'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
